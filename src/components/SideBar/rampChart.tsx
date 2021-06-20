cda4aea9204d6e75fbc8578566afffd1765f6e58
Shaun Li <2711456@gmail.com>
2021-06-21 05:14:38 +1000

    ramp chart layout

--- src/components/SideBar/rampChart.tsx
+++ src/components/SideBar/rampChart.tsx
@@ -1,43 +1,47 @@
 import "./index.scss";
 import React from "react";
 import { PieChart, Pie, Cell } from "recharts";
-
-const data = [
-  { name: "Group A", value: 400 },
-  { name: "Group B", value: 300 },
-  { name: "Group C", value: 300 },
-  { name: "Group D", value: 200 }
-];
-
-const COLORS = ['#00B8AA', '#45DAD2', '#A7E5E0', '#ffffff'];
-
-const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({
-	cx,
-	cy,
-	midAngle,
-	innerRadius,
-	outerRadius,
-	percent,
-}: any) => {
-	const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-	const x = cx + 1.3 * radius * Math.cos(-midAngle * RADIAN);
-	const y = cy + 1.3 * radius * Math.sin(-midAngle * RADIAN);
-
-  return (
-	<text
-		x={x}
-		y={y}
-		fill="#3EDBD2"
-		textAnchor={x > cx ? "start" : "end"}
-		dominantBaseline="central"
-	>
-		{`${(percent * 100).toFixed(0)}%`}
-	</text>
-  );
-};
+/**
+ * @description A pure component to render the ramp chart
+ */
 
 export default function RampChart() {
+	// temp mock ramp data
+	const data = [
+		{ name: "Group A", value: 400 },
+		{ name: "Group B", value: 300 },
+		{ name: "Group C", value: 300 },
+		{ name: "Group D", value: 200 }
+	];
+	
+	const COLORS = ['#00B8AA', '#45DAD2', '#A7E5E0', '#ffffff'];
+	
+	const RADIAN = Math.PI / 180;
+	const renderCustomizedLabel = ({
+		cx,
+		cy,
+		midAngle,
+		innerRadius,
+		outerRadius,
+		percent,
+		}: any) => {
+			const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+			const x = cx + 1.3 * radius * Math.cos(-midAngle * RADIAN);
+			const y = cy + 1.3 * radius * Math.sin(-midAngle * RADIAN);
+		
+		return (
+			<text
+				x={x}
+				y={y}
+				fill="#3EDBD2"
+				textAnchor={x > cx ? "start" : "end"}
+				dominantBaseline="central"
+			>
+				{`${(percent * 100).toFixed(0)}%`}
+			</text>
+		);
+	};
+
 	return (
 		<div className='pieChart'>
 			<PieChart width={240} height={240}>
@@ -60,6 +64,5 @@ export default function RampChart() {
 				Ramp algorithm blablabla
 			</div>
 		</div>
-		
 	);
 }
